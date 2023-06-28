4f03afcc98f63ed4a7253c91a58770dc051f083e
19010-HFHS <81333059+19010-HFHS@users.noreply.github.com>
2023-06-28 01:57:44 +0000

    added second version of data input component

--- data_input_v2.py
+++ data_input_v2.py
@@ -5,6 +5,15 @@ from tkinter import *
 class analyse:
 
   def __init__(self):
+
+    # Sets up feedback format function
+    self.var_feedback = StringVar()
+    self.var_feedback.set("")
+
+    # Sets up has error format function
+    self.var_has_error = StringVar()
+    self.var_has_error.set("no")
+    
     # Set up GUI frame
     self.scan_frame = Frame(padx=10, pady=10)
     self.scan_frame.grid()
@@ -13,7 +22,7 @@ class analyse:
     self.scan_heading = Label(self.scan_frame,
                               text="Bias Scanner",
                               font=("Arial", "16", "bold"),
-                              bg="#006400")
+                              bg="#AEF359")
 
     self.scan_heading.grid(row=0)
 
@@ -33,7 +42,7 @@ class analyse:
 
     # Set up error message
     error = "please enter percentages equal to 100%, may not include negatives, decimals or non-numerical subjects"
-    self.scan_error = Label(self.scan_frame, text=error, fg="#9C0000")
+    self.scan_error = Label(self.scan_frame, text="", fg="#9C0000")
     self.scan_error.grid(row=3)
 
     # Set up analyse button
@@ -42,15 +51,36 @@ class analyse:
 
     self.analyse_button = Button(self.button_frame,
                                  text="Analyse",
-                                 bg="#004C99",
+                                 bg="#2A9DF4",
                                  fg="#FFFFFF",
                                  width=12)
 
     self.analyse_button.grid(row=0, column=0, padx=5, pady=5)
 
+  def valid_check(self, max_value):
+    # Defines error message
+    has_error = "no"
+    error = "please enter percentages equal to {}%, may not include negatives, decimals or non-numerical subjects".format(max_value)
 
+    # Checks that the user enters a valid number
+    response = self.temp_entry.get()
+    try:
+      response = int(response)
+      # Checks if value is above maximum
+      if response > max_value:
+        has_error = "yes"
+
+      # Sets has error format function so that entry box and labels can be correctly formatted by formatting function
+      if has_error == "yes":
+        self.var_has_error.set("yes")
+        self.var_feedback.set(error)
+        return "invalid"
+      else:
+        return response
+      
+  
 # **** Main Routine ****
-if __name__ == "__main__":
+
   root = Tk()
   root.title("Bias Scanner")
   analyse()
