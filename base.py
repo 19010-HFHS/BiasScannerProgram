b6a0ac21f9d63400d0ef7a3d5823b1e9993642f6
19010-HFHS <81333059+19010-HFHS@users.noreply.github.com>
2023-08-01 01:28:27 +0000

    updated base component

--- base.py
+++ base.py
@@ -1,6 +1,13 @@
 # Set up imports
 from tkinter import *
 
+# Set up lists
+all_input = []
+gender = ""
+ethnicity = ""
+major_percentage = ""
+minor_percentage = ""
+
 
 class analyse:
 
@@ -13,7 +20,7 @@ class analyse:
     # Sets up has error format function
     self.var_has_error = StringVar()
     self.var_has_error.set("no")
-    
+
     # Set up GUI frame
     self.scan_frame = Frame(padx=10, pady=10)
     self.scan_frame.grid()
@@ -27,22 +34,22 @@ class analyse:
     self.scan_heading.grid(row=0)
 
     # Set up instructions
-    instructions = "Please enter your data in the format of two percentages that are equal to 100%"
+    instructions = "Please enter the author's gender, ethnicity, and the subject of research for their major percentage and minor percentage, press Analyse after each input"
     self.scan_instructions = Label(self.scan_frame,
                                    text=instructions,
                                    wrap=250,
                                    width=40,
-                                   justify="left")
+                                   justify="center")
 
     self.scan_instructions.grid(row=1)
 
     # Set up input box
     self.scan_input = Entry(self.scan_frame, font=("Arial", "14"))
-    self.scan_input.grid(row=2, padx=10, pady=10)
+    self.scan_input.grid(row=3, column=0, padx=10, pady=10)
 
     # Set up error message
-    self.scan_output = Label(self.scan_frame, text="", fg="#9C0000")
-    self.scan_output.grid(row=3)
+    self.scan_output = Label(self.scan_frame, text="", wrap=250, fg="#9C0000")
+    self.scan_output.grid(row=5)
 
     # Set up analyse button
     self.button_frame = Frame(self.scan_frame)
@@ -52,48 +59,54 @@ class analyse:
                                  text="Analyse",
                                  bg="#2A9DF4",
                                  fg="#FFFFFF",
-                                 width=12, command=lambda: self.analysis(100, 0))
+                                 width=12,
+                                 command=lambda: self.analysis())
 
     self.analyse_button.grid(row=0, column=0, padx=5, pady=5)
 
-  def valid_check(self, max_value, min_value):
+  def valid_check(self):
     # Defines error message
     has_error = "no"
-    error = "please enter percentages equal to {}%, may not include negatives, decimals or non-numerical subjects".format(max_value)
-
-    # Checks that the user enters a valid number
-    response = self.scan_input.get()
+    error = "Invalid Format: please enter please enter the authors gender and ethnicity, as well as the subject of research for their major and minor percentage"
+    
+    # Sets has error format function so that entry box and labels can be correctly formatted by formatting function
     try:
-      response = int(response)
-      # Checks if value is above maximum
-      if response > max_value:
-        has_error = "yes"
-      elif response < min_value:
-        has_error = "yes"
-      else:
-        pass
-    # Checks for value error
-    except ValueError:
+      float(self.scan_input.get())
       has_error = "yes"
-      
-      # Sets has error format function so that entry box and labels can be correctly formatted by formatting function
+
+    except ValueError:
+      has_error = "no"
+    
     if has_error == "yes":
       self.var_has_error.set("yes")
       self.var_feedback.set(error)
       return "invalid"
-      
-    elif has_error == "no":
-      return response
 
-  def analysis(self, max_value, min_value):
-    to_analyse = self.valid_check(max_value, min_value)
+    else:
+      pass
+
+  def analysis(self):
+    to_analyse = self.valid_check()
     answer = ""
 
     # Run analysis
-    if max_value == 100 and min_value == 0:
+    if self.valid_check() != "invalid":
       # Replace once analysis code is complete
-      answer = "Analysis Complete!"
-    
+      response = self.scan_input.get()
+      all_input.append(response)
+      self.scan_input.delete(0, END)
+      if len(all_input) == 4:
+        gender = all_input[0]
+        ethnicity = all_input[1]
+        major_percentage = all_input[2]
+        minor_percentage = all_input[3]
+        
+        if gender.lower() == major_percentage.lower() or ethnicity.lower() == major_percentage.lower():
+          answer = "Possible Confirmation Bias detected!!!\n Possible Selection Bias detected!!!\n Possible Observer Bias detected!!!"
+
+        if gender.lower() == minor_percentage.lower() or ethnicity.lower() == minor_percentage.lower():
+          answer = "Possible Confirmation Bias detected!!!\n Possible Selection Bias detected!!!\n Possible Observer Bias detected!!!"
+
     # if valid check returns invalid set feedback to no
     if to_analyse == "invalid":
       # Red text, Pink entry box
@@ -102,11 +115,12 @@ class analyse:
       self.scan_output.config(text=self.var_feedback.get())
 
     else:
-      output = answer
-      self.scan_output.config(fg="#004C88")
+      output = (all_input, answer)
+      self.scan_output.config(fg="#CC7722")
       self.scan_input.config(bg="#FFFFFF")
       self.scan_output.config(text=output)
-      
+
+
 # **** Main Routine ****
 if __name__ == "__main__":
   root = Tk()
